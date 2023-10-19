d6880260c1b649104b93905623e7e76040fb958f
cabelstein12 <Caleb.Steinwandt@gmail.com>
2023-10-19 10:08:28 -0600

    <type>[optional_scope]: <Add working buttons to play individual rounds>
    [optional_body]
    
    [optional_footer]

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const randomSel = ['rock', 'paper', 'scissors'];
-const playerSelection = prompt("Enter your choice (rock, paper, or scissors)").toLowerCase();
+let playerSelection;
+let computerSelection;
+
+
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
+
+rockBtn.addEventListener('click', () => {
+    playerSelection = 'rock';
+    // getComputerChoice();
+    computerSelection = getComputerChoice()
+    playRound(playerSelection, computerSelection);
+})
+
+paperBtn.addEventListener('click', () => {
+    playerSelection = 'paper';
+    computerSelection = getComputerChoice()
+    playRound(playerSelection, computerSelection);
+})
+
+scissorsBtn.addEventListener('click', () => {
+    playerSelection = 'scissors';
+    computerSelection = getComputerChoice()
+    playRound(playerSelection, computerSelection);
+})
 
 function getComputerChoice() {
     return randomSel[Math.floor(Math.random() * randomSel.length)];
@@ -32,33 +57,33 @@ function playRound(playerSelection, computerSelection) {
 let playerScore = 0;
 let pcScore = 0;
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Enter your choice (rock, paper, or scissors)").toLowerCase();
-        if (!['rock', 'paper', 'scissors'].includes(playerSelection)) {
-            alert('Invalid input. Please choose rock, paper, or scissors.');
-            continue;
-        }
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         const playerSelection = prompt("Enter your choice (rock, paper, or scissors)").toLowerCase();
+//         if (!['rock', 'paper', 'scissors'].includes(playerSelection)) {
+//             alert('Invalid input. Please choose rock, paper, or scissors.');
+//             continue;
+//         }
         
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection, computerSelection);
+//         const computerSelection = getComputerChoice();
+//         const result = playRound(playerSelection, computerSelection);
         
-        if (result === 'You Win') {
-            playerScore += 1;
-        } else if (result === 'You Lose') {
-            pcScore += 1;
-        }
+//         if (result === 'You Win') {
+//             playerScore += 1;
+//         } else if (result === 'You Lose') {
+//             pcScore += 1;
+//         }
         
-        alert(`Round ${i + 1}: ${result}\nPlayer Score: ${playerScore}\nComputer Score: ${pcScore}`);
-    }
+//         alert(`Round ${i + 1}: ${result}\nPlayer Score: ${playerScore}\nComputer Score: ${pcScore}`);
+//     }
     
-    if (playerScore === 5) {
-        return "You've Won";
-    } else if (pcScore === 5) {
-        return "You've Lost";
-    } else {
-        return "Game Over";
-    }
-}
+//     if (playerScore === 5) {
+//         return "You've Won";
+//     } else if (pcScore === 5) {
+//         return "You've Lost";
+//     } else {
+//         return "Game Over";
+//     }
+// }
 
-alert(game());
+//alert(game());
